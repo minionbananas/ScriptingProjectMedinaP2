2661b26d6b24ab8986650612bb53265a78a7e188
Anthonyggs <anthony102210@icloud.com>
2025-01-09 09:12:08 -0800

    created a script
    the active object script and the check state

---
+++ ScriptingProjectP2Medina/Assets/Scripts/CheckState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject MyObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Active Self:" + MyObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + MyObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
