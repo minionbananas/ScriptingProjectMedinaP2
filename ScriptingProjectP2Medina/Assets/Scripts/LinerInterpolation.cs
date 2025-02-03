9cb85e73e89bb025fb6ee08bec3dbaf84e646cce
Anthonyggs <anthony102210@icloud.com>
2025-02-03 09:30:15 -0800

    liner interpolation
    made a liner interpolation script

---
+++ ScriptingProjectP2Medina/Assets/Scripts/LinerInterpolation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinerInterpolation : MonoBehaviour
+{/*
+  *
+  *
+  // In this case, result = 4
+    float result = Math.Lerp 3(, 5, 0.5);
+
+    Vector3 from = new Vector3 (1f ,2f, 3f);
+    Vector3 to = new Vector3.Lerp (5f, 6f, 7f);
+
+    // Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (form, to, 0.75f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
