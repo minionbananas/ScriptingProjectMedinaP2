a0e299a3c66bda694c3243e2ad3069c08f570216
Anthonyggs <anthony102210@icloud.com>
2025-03-18 09:39:57 -0700

    get component
    made 3 scripts

---
+++ ScriptingProjectP2Medina/Assets/Scripts/UsingOtherComponents.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour
+{
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherscript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+    void Awake ()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObject.GetComponent<BoxCollider>(); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayerDeaths + " times");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
